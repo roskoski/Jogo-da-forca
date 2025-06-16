226fc9d1f67a91e40830e811ed791b1a7cf7ba60
Eduardo Roskoski <eduardo.roskoski.oliveira@escola.pr.gov.br>
2025-06-16 08:41:16 -0300

    script

---
+++ script.js
@@ -0,0 +1,11 @@
+const words = ["javascript","html","css","programa","computador","desenvolvimento"];
+let selectWord ="";
+let correctLetters = [];
+let wrongLetters =[];
+let maxAttempts = 6;
+
+function startGame(){
+    selectWord = words[Math.floor(Math.random() * words.length)];
+
+
+}
