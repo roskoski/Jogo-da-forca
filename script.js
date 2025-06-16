f43a0e229d6a82241ea93a33f96d1ff14a313442
Eduardo Roskoski <eduardo.roskoski.oliveira@escola.pr.gov.br>
2025-06-16 08:48:51 -0300

    script.js

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ let maxAttempts = 6;
 
 function startGame(){
     selectWord = words[Math.floor(Math.random() * words.length)];
-
-
+    correctLetters = [];
+    wrongLetters = [];
+    document.getElementById("message").textContent = "";
+    displayWord();
+    generateLetterButttons();
 }
